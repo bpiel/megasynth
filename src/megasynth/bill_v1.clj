61142485962a3ce1d5f809a2991c9684d4cfe285
Bill Piel <bill@billpiel.com>
2025-07-05 12:05:50 -0400

    atw progress

--- src/megasynth/bill_v1.clj
+++ src/megasynth/bill_v1.clj
@@ -159,14 +159,7 @@
 (swap! synths-ifaces&
        assoc :volca-keys0
        (mk-synth-iface
-           [                ;; LFO
-            lfo-rate [0.1 5.0 10.0]    ;; Hz
-            lfo-depth [0.0 1.0 3.0]   ;; global depth scaler
-            lfo->pitch [0.0 0.0 1.0]  ;; amount in semitones
-            lfo->cutoff [0.0 0.0 1.0] ;; amount added to cutoff
-            lfo->amp [0.0 0.0 1.0]    ;; amplitude modulation depth
-
-            amp [0.0 0.9 2.0]
+           [amp [0.0 0.9 2.0]
             detune [0.0 0.0 1.0]
 
             ;; Filter
@@ -179,8 +172,15 @@
             decay [0.0 0.2 3.0]
             sustain [0.0 0.5 3.0]
             release [0.0 0.3 3.0]
-            
-            ;; Delay
+
+            ;; LFO
+            lfo-rate [0.1 5.0 10.0]    ;; Hz
+            lfo-depth [0.0 1.0 3.0]   ;; global depth scaler
+            lfo->pitch [0.0 0.0 1.0]  ;; amount in semitones
+            lfo->cutoff [0.0 0.0 1.0] ;; amount added to cutoff
+            lfo->amp [0.0 0.0 1.0]    ;; amplitude modulation depth
+
+                        ;; Delay
             delay-time [0.0 0.0 2.0]
             delay-fb [0.0 0.4 2.0]
             delay-mix [0.0 0.3 1.0]
@@ -267,7 +267,132 @@
 #_
 (play around-the-world-lead 1000)
 
+
+(o/defsynth around-the-world-lead-2
+  [freq 110
+   gate 1
+
+   ;; Amp
+   amp 0.8
+   pan 0.0
+
+   ;; Detune and pulse width
+   detune 0.02
+   pw1 0.39        ;; ~9:30 clock (range 0.0 to 1.0)
+   pw2 0.5         ;; ~12:05 clock
+
+   ;; Filter
+   cutoff 400
+   res 0.5
+   env-amt 800
+   svf-mix 0.7     ;; blend between osc and filtered
+
+   ;; Filter envelope
+   f-env-att 0.0
+   f-env-dec 0.02
+   f-env-sus 0.0
+   f-env-rel 0.0
+
+   ;; Amp envelope
+   a-env-att 0.0
+   a-env-dec 0.02
+   a-env-sus 0.6
+   a-env-rel 0.1
+
+   ;; LFO for PWM
+   lfo-rate 6.0
+   lfo-depth 0.05]
+  (let [lfo (o/lf-tri lfo-rate)
+        ;; Oscillators
+        f1 (* freq (ugen-semitone-ratio (* -1.0 detune)))
+        f2 (* freq (ugen-semitone-ratio detune))
+        pulse1 (o/pulse f1 (+ pw1 (* lfo lfo-depth)))
+        pulse2 (o/pulse f2 (+ pw2 (* lfo lfo-depth)))
+        saw1 (o/saw f1)
+        saw2 (o/saw f2)
+        mixed-osc (o/mix [(* 0.6 saw1) (* 0.6 saw2) (* 0.4 pulse1) (* 0.4 pulse2)])
+
+        ;; Filter envelope
+        f-env (o/env-gen (o/adsr f-env-att f-env-dec f-env-sus f-env-rel) gate)
+        mod-cutoff (+ cutoff (* env-amt f-env))
+
+        ;; Filtered tone
+        svf (o/rlpf mixed-osc mod-cutoff res)
+        filtered (o/x-fade2 mixed-osc svf svf-mix)
+
+        ;; Amp envelope
+        amp-env (o/env-gen (o/adsr a-env-att a-env-dec a-env-sus a-env-rel) gate :action o/FREE)
+        final (* filtered amp-env amp)
+
+        ;; Output
+        out (o/pan2 final pan)]
+    (o/out 0 out)))
+
+(play around-the-world-lead-2 600)
+
+
 (o/stop)
+
+
+(swap! synths-ifaces&
+       assoc :atw-lead-1
+       (mk-synth-iface
+           [ ;; Amp
+            amp [0.0 0.8 2.0]
+            pan [-1.0 0.0 1.0]
+
+            ;; Detune and pulse width
+            detune [0.0 0.02 0.2]
+            pw1 [0.0 0.39 0.8] ;; ~9:30 clock (range 0.0 to 1.0)
+            pw2 [0.0 0.5 1.0]  ;; ~12:05 clock
+
+            ;; Filter
+            cutoff [0.0 400 4000]
+            res [0.0 0.5 1.0]
+            env-amt [0.0 800 2000]
+            svf-mix [0.0 0.7 2.0] ;; blend between osc and filtered
+
+            ;; Filter envelope
+            f-env-att [0.0 0.0 1.0]
+            f-env-dec [0.0 0.02 0.2]
+            f-env-sus [0.0 0.0 1.0]
+            f-env-rel [0.0 0.0 1.0]
+
+            ;; Amp envelope
+            a-env-att [0.0 0.0 2.0]
+            a-env-dec [0.0 0.02 0.2]
+            a-env-sus [0.0 0.6 2.0]
+            a-env-rel [0.0 0.1 1.0]
+
+            ;; LFO for PWM
+            lfo-rate [0.0 6.0 12.0]
+            lfo-depth [0.0 0.05 0.1]]
+           (let [lfo (o/lf-tri lfo-rate)
+                 ;; Oscillators
+                 f1 (* freq (ugen-semitone-ratio (* -1.0 detune)))
+                 f2 (* freq (ugen-semitone-ratio detune))
+                 pulse1 (o/pulse f1 (+ pw1 (* lfo lfo-depth)))
+                 pulse2 (o/pulse f2 (+ pw2 (* lfo lfo-depth)))
+                 saw1 (o/saw f1)
+                 saw2 (o/saw f2)
+                 mixed-osc (o/mix [(* 0.6 saw1) (* 0.6 saw2) (* 0.4 pulse1) (* 0.4 pulse2)])
+
+                 ;; Filter envelope
+                 f-env (o/env-gen (o/adsr f-env-att f-env-dec f-env-sus f-env-rel) gate)
+                 mod-cutoff (+ cutoff (* env-amt f-env))
+
+                 ;; Filtered tone
+                 svf (o/rlpf mixed-osc mod-cutoff res)
+                 filtered (o/x-fade2 mixed-osc svf svf-mix)
+
+                 ;; Amp envelope
+                 amp-env (o/env-gen (o/adsr a-env-att a-env-dec a-env-sus a-env-rel) gate :action o/FREE)
+                 final (* filtered amp-env amp)
+
+                 ;; Output
+                 out (o/pan2 final pan)]
+             (o/out 0 out))
+           {:mono? true}))
 
 (o/defsynth volca-keys0
   [freq 440
@@ -524,6 +649,7 @@
       14 (set-synth! :da-funk)
       15 (set-synth! :final-countdown)
       16 (set-synth! :volca-keys0)
+      17 (set-synth! :atw-lead-1)      
       (change-synth-arg! (- data1 20) data2))
     (catch Throwable e
       (clojure.pprint/pprint e))))
